0d2d4a8ead977d78bafce9e18731141302087f02
Etienne Samson <samson.etienne@gmail.com>
2025-11-04 00:43:25 +0100

    Enable stats viewing if the server supports it

--- src/patches/index.ts
+++ src/patches/index.ts
@@ -14,6 +14,7 @@ import highPerfWebGL from './highPerfWebGL';
 import placeSpawnDefaultOff from './placeSpawnDefaultOff';
 import portalInfo from './portalInfo';
 import removeTracking from './removeTracking';
+import serverStats from './serverStats';
 import stripAws from './stripAws';
 import terrainTilesProfileView from './terrainTilesProfileView';
 
@@ -29,6 +30,7 @@ const patches: AnyPatch[] = [
     fixAlphaMapBounds,
     formatMarketNumbers,
     highPerfWebGL,
+    serverStats,
     // Must be last
     beautify,
 ];
