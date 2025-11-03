4ee8837b71822599e36a93f0c23ac0f9cba80cdb
Etienne Samson <samson.etienne@gmail.com>
2025-11-04 00:40:01 +0100

    Force high-performance mode on the WebGL context

---
+++ src/patches/highPerfWebGL.ts
@@ -0,0 +1,13 @@
+import { applyPatch, Patch } from './helpers';
+
+const patch: Patch = {
+    id: 'high-performance-webgl',
+    description: 'Forces WebGL canvas to use high-performance mode.',
+    disabled: true,
+    match: (url: string) => url === 'vendor/renderer/renderer.js',
+    async apply(src: string) {
+        src = applyPatch(src, 's.options.powerPreference', '"high-performance"');
+        return src;
+    },
+};
+export default patch;
